e6d1859c1644bb6454ea4df77cdcfb2c9be564cc
sakroutamine <80228589+sakroutamine@users.noreply.github.com>
2026-04-10 16:02:11 -0700

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        checkpoint=len(nums)-1
+            
+        for i in range(checkpoint,-1,-1):
+            print(i, checkpoint)
+            if i+nums[i] >= checkpoint:
+                checkpoint = i
+        return checkpoint ==0
+            
+
+
+
