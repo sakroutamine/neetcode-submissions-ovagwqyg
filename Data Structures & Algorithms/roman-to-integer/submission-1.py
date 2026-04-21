6ac92446bc2d3e41c9c3ef7bcfae092136e9553a
sakroutamine <80228589+sakroutamine@users.noreply.github.com>
2026-04-21 09:38:32 -0700

    Add: roman-to-integer - submission-1

---
+++ Data Structures & Algorithms/roman-to-integer/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        maps = {'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        total = 0 
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and maps[s[i]] < maps[s[i + 1]]:
+                total -= maps[s[i]]
+            else:
+                total += maps[s[i]]
+        return total 
